f262815fc154ff5ef581a058c1840a97ba473ad1
José Rodrigo Lemos Pinto <rodlemoss@gmail.com>
2020-08-04 12:10:35 -0400

    View Encapsulation Architecture

---
+++ TinderLBTA/Views/HomeBottomControlsStackView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeBottomControlsStackView.swift
+//  TinderLBTA
+//
+//  Created by Rodrigo Lemos on 04/08/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+class HomeBottomControlsStackView: UIStackView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        distribution = .fillEqually
+        heightAnchor.constraint(equalToConstant: 100).isActive = true
+         
+        let subviews = [#imageLiteral(resourceName: "refresh_circle"), #imageLiteral(resourceName: "dismiss_circle"), #imageLiteral(resourceName: "super_like_circle"), #imageLiteral(resourceName: "like_circle"), #imageLiteral(resourceName: "boost_circle")].map { (img) -> UIView in
+            let button = UIButton(type: .system)
+            button.setImage(img.withRenderingMode(.alwaysOriginal), for: .normal)
+            return button
+        }
+         
+        subviews.forEach { (v) in
+            addArrangedSubview(v)
+        }
+    }
+     
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
