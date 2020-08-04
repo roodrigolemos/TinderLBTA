70ff823b3aa58d4985baa4fcf0514f5d6bfcf3b6
José Rodrigo Lemos Pinto <rodlemoss@gmail.com>
2020-08-04 10:39:14 -0400

    Initial Commit

---
+++ TinderLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderLBTA
+//
+//  Created by Rodrigo Lemos on 04/08/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
