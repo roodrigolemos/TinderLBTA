f3b326da8685392d17d0abd28ae3a9c24f1439c2
José Rodrigo Lemos Pinto <rodlemoss@gmail.com>
2020-08-06 13:17:03 -0400

    Intro MVVM Card View Models

---
+++ TinderLBTA/ViewModels/CardViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  CardViewModel.swift
+//  TinderLBTA
+//
+//  Created by Rodrigo Lemos on 06/08/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    // we'll define the properties that are view will display/render out
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
+
+// what exactly do we do with this card view model thing???
+
