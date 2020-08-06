325cb5c9c6db1c83ca31a2a13f3d75ef4a6e8d05
José Rodrigo Lemos Pinto <rodlemoss@gmail.com>
2020-08-06 12:59:20 -0400

    Multiple Cards and User Model

--- TinderLBTA/HomeController.swift
+++ TinderLBTA/HomeController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
     
     let topStackView = TopNavigationStackView()
     let cardsDeckView = UIView()
     let buttonsStackView = HomeBottomControlsStackView()
+    
+    let users = [
+        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c"),
+        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +26,21 @@ class ViewController: UIViewController {
     }
     
     fileprivate func setupDummyCards() {
-        print("Setting up dummy cards")
-        let cardView = CardView(frame: .zero)
-        cardsDeckView.addSubview(cardView)
-        cardView.fillSuperview()
+        users.forEach { (user) in
+            let cardView = CardView(frame: .zero)
+            cardView.imageView.image = UIImage(named: user.imageName)
+            cardView.informationLabel.text = "\(user.name) \(user.age)\n\(user.profession)"
+            
+            let attributedText = NSMutableAttributedString(string: user.name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
+            attributedText.append(NSAttributedString(string: "  \(user.age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
+            
+            attributedText.append(NSAttributedString(string: "\n\(user.profession)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
+            
+            cardView.informationLabel.attributedText = attributedText
+            
+            cardsDeckView.addSubview(cardView)
+            cardView.fillSuperview()
+        }
     }
 
     // MARK:- Fileprivate
---
+++ TinderLBTA/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  TinderLBTA
+//
+//  Created by Rodrigo Lemos on 06/08/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    // defining our properties for our model layer
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+}
