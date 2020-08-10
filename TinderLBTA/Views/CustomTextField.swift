5114cd8dbe8c462fa6839bdad557a06722abdb8f
Rodrigo Lemos <rodlemoss@gmail.com>
2020-08-10 13:37:16 -0400

    Creating a Registration Page

---
+++ TinderLBTA/Views/CustomTextField.swift
@@ -0,0 +1,38 @@
+//
+//  CustomTextField.swift
+//  TinderLBTA
+//
+//  Created by Rodrigo Lemos on 10/08/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    let padding: CGFloat
+    let height: CGFloat
+    
+    init(padding: CGFloat, height: CGFloat) {
+        self.padding = padding
+        self.height = height
+        super.init(frame: .zero)
+        layer.cornerRadius = height / 2
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: height)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
