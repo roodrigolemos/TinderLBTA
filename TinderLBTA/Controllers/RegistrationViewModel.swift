d3b1948e1a34eb88f8b31389d9a29d5a286842c2
Rodrigo Lemos <rodlemoss@gmail.com>
2020-08-17 09:06:01 -0400

    Registration View Model

---
+++ TinderLBTA/Controllers/RegistrationViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationViewModel.swift
+//  TinderLBTA
+//
+//  Created by Rodrigo Lemos on 17/08/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewModel {
+    
+    var fullName: String? {
+        didSet {
+            checkFormValidity()
+        }
+    }
+    var email: String? { didSet { checkFormValidity() } }
+    var password: String? { didSet { checkFormValidity() } }
+    
+    fileprivate func checkFormValidity() {
+        let isFormValid = fullName?.isEmpty == false && email?.isEmpty == false && password?.isEmpty == false
+        isFormValidObserver?(isFormValid)
+    }
+    
+    // Reactive programming
+    var isFormValidObserver: ((Bool) -> ())?
+    
+}
+
