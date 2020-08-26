bd2b6fecb29049719bdea2f88c875429a4509df4
Rodrigo Lemos <rodlemoss@gmail.com>
2020-08-26 08:58:32 -0400

    settings for rows

---
+++ TinderLBTA/Views/SettingsCell.swift
@@ -0,0 +1,46 @@
+//
+//  SettingsCell.swift
+//  TinderLBTA
+//
+//  Created by Rodrigo Lemos on 26/08/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    
+    class SettingsTextField: UITextField {
+        
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 44)
+        }
+    }
+    
+    let textField: UITextField = {
+        let tf = SettingsTextField()
+        tf.placeholder = "Enter Name"
+        return tf
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(textField)
+        textField.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+}
+
